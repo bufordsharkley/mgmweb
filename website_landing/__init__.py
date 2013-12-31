af5f4533827e090b465d681aec5b2c139cf4f907
Mark Mollineaux <bufordsharkley@gmail.com>
2013-12-30 23:08:37 -0500

    Ugh, application not app

--- website_landing/__init__.py
+++ website_landing/__init__.py
@@ -1,14 +1,14 @@
 import flask
  
-application = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
 #Set application.debug=true to enable tracebacks on Beanstalk log output. 
 #Make sure to remove this line before deploying to production.
-application.debug=True
+app.debug=True
  
-@application.route('/')
+@app.route('/')
 def hello_world():
     return "Hello world, running as package!"
  
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
