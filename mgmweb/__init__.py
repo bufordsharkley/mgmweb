1970b978a28f1cff2f2a9881f00592251b586af3
Mark Mollineaux <bufordsharkley@gmail.com>
2014-02-25 23:25:20 -0800

    Drawing is working, almost entirely. Metadata mostly there

--- mgmweb/__init__.py
+++ mgmweb/__init__.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, abort
+import datetime
+import random
+
+from flask import Flask, render_template, abort, redirect, url_for
 from jinja2 import TemplateNotFound
+
+from .drawing_metadata import metadata
 
 application = app = Flask(__name__)
 
@@ -15,6 +20,33 @@ def subpage(path):
         return render_template(path + '.html')
     except TemplateNotFound:
         abort(404)
+
+
+@app.route('/drawing/<int:num>/')
+def drawing(num):
+    try:
+        metadatum = sorted(metadata, key=lambda k: k['number'])[num-1]
+    except IndexError:
+        abort(404)
+    return render_template('drawing.html',
+                           number=metadatum['number'],
+                           small_src=metadatum['image']['src_small'],
+                           date=metadatum['date'],
+                           caption=metadatum.get('caption',''),
+                           additionaltext=metadatum.get('wordsinimage',''))
+
+
+@app.route('/drawing/')
+@app.route('/drawing/random/')
+def random_drawing():
+    metadatum = metadata[random.randrange(len(metadata)) + 1]
+    return redirect(url_for('drawing', num=random.randrange(len(metadata))))
+
+
+@app.route('/hackers.txt')
+def hackers():
+    return 'site has been hacked at time: {}'.format(datetime.datetime.now())
+
 
 @app.errorhandler(404)
 def page_not_found(e):
