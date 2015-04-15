4f4fa502b10735eb0d27d549c6ec4ea018e64a2d
Mark Mollineaux <bufordsharkley@gmail.com>
2015-04-15 15:56:28 -0700

    shamble more before it shamble less

--- mgmweb/static/friendstxtwebringgenerator.js
+++ mgmweb/static/friendstxtwebringgenerator.js
@@ -1,24 +1,37 @@
 console.log('hey');
+var friendstxt = null;
 
 function getJSON(path, callback) {
-
     var xmlhttp = new XMLHttpRequest();                 
     xmlhttp.overrideMimeType('application/json');  
-
     xmlhttp.onreadystatechange = function() {
         ready = (xmlhttp.readyState == 4 && xmlhttp.status == 200);
-        callback(ready ? xmlhttp.responseText : false);
+        callback(ready ? xmlhttp.responseText.split("\n") : false);
     };
-
     xmlhttp.open('GET', path, true);
     xmlhttp.send();
-
 };
 
 
 getJSON('friends.txt', function(data){
-console.log(data)});
+    if (data){
+        // note: hardcoded line indices. nope.
+        for (var ii=4; ii < data.length - 3; ii++){
+            var friendline = removeComments(data[ii]);
+            var split = friendline.split(',');
+            var name = split.slice(0, -1).join(',');
+            var url = split[split.length - 1];
+            jsonphack.getJSON(url + '/friends.txt');
+            console.log(name);
+            console.log(url);
+            console.log(friendstxt);
+        }
+    }
+});
 
+function removeComments(string){
+    return string.split('#')[0];
+}
 
 var jsonphack = {  
     currentScript: null,  
