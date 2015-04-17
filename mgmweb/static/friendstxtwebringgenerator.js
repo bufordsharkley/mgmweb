8ae1db648995d41cc2e255046f2b5bdc525d5a87
Mark Mollineaux <bufordsharkley@gmail.com>
2015-04-16 19:22:53 -0700

    Hmm?

--- mgmweb/static/friendstxtwebringgenerator.js
+++ mgmweb/static/friendstxtwebringgenerator.js
@@ -20,11 +20,16 @@ getJSON('friends.txt', function(data){
             var friendline = removeComments(data[ii]);
             var split = friendline.split(',');
             var name = split.slice(0, -1).join(',');
-            var url = split[split.length - 1];
+            var url = split[split.length - 1].replace(/^\s+|\s+$/g, '');
+            var url = url + '/friends.txt';
+            var callback = function(d){ console.log(d); alert(d.attribute_name)};
+            J50Npi.getJSON(url, {}, callback);
+            /*
             jsonphack.getJSON(url + '/friends.txt');
             console.log(name);
             console.log(url);
             console.log(friendstxt);
+            */
         }
     }
 });
@@ -33,17 +38,44 @@ function removeComments(string){
     return string.split('#')[0];
 }
 
+var J50Npi = {  
+    currentScript: null,  
+    getJSON: function(url, data, callback) {
+      var src = url + (url.indexOf("?")+1 ? "&" : "?");
+      var head = document.getElementsByTagName("head")[0];
+      var newScript = document.createElement("script");
+      var params = [];
+      var param_name = ""
+
+      this.success = callback;
+      data["callback"] = "J50Npi.success";
+      for(param_name in data){  
+          params.push(param_name + "=" + encodeURIComponent(data[param_name]));  
+      }
+      src += params.join("&")
+
+      newScript.type = "text/javascript";  
+      newScript.src = src;
+
+      if(this.currentScript) head.removeChild(this.currentScript);
+      head.appendChild(newScript);
+      this.currentScript = newScript;
+    },
+    success: null
+}; 
+
 var jsonphack = {  
     currentScript: null,  
-    getJSON: function(url) {
+    getJSON: function(url, callback) {
       var head = document.getElementsByTagName("head")[0];
       var newScript = document.createElement("script");
 
       newScript.type = "text/javascript";  
       newScript.src = url;
 
-      if(this.currentScript) head.removeChild(currentScript);
-      head.appendChild(newScript); 
+      if(this.currentScript) head.removeChild(this.currentScript);
+      head.appendChild(newScript);
+      this.currentScript = newScript;
     },
     success: null
 }; 
