847d85a39004580cdfe87624205abdac22f240fc
Mark Mollineaux <bufordsharkley@gmail.com>
2017-08-13 19:00:09 -0700

    Better freeze

---
+++ static/markdown/poems/vagrant.md
@@ -0,0 +1,35 @@
+##A Message for a Vagrant
+
+I’m sorry, sir, vagrant,
+But we’ve no room to let
+Forgive!
+(If you deem it wise)
+But mind, kind sir,
+That we don’t choose not, but CAN’T
+Yes:
+We’ve bought barrels
+And barrels
+Of brine
+
+The salesman came morn, with his brine-tastic offer
+Yes!
+Pickling, and pickling galore!
+At just seven dollars apiece
+(Per barrel)
+We shant’d give scorn
+No! Bought us, did, eighty-three!
+
+Storage came tricky:
+Barrels huge, house less so
+But cram!
+And lug!
+‘Til each room barreled be
+Yes, each bedroom, parlor and den
+
+Now think of the danger that face each of we:
+I, my wife, and the kids
+Chance o’ pickling death, big threat already is,
+Would worsen
+With one vagrant in
+
+[//]: # (METAyear:2004)
---
+++ static/splashajax.js
@@ -0,0 +1,35 @@
+function loadForFrontPage() {
+  loadAllDescriptions();
+  $('.initiator').on('mouseenter mouseleave', function(e) {
+    if (e.type === "mouseleave"){
+      loadSummary('default');
+    }else{
+      loadSummary(this.id);
+    }
+    //$('.receiver').trigger(e.type);
+  })  
+}
+
+function loadAllDescriptions() {
+  $.getJSON($SCRIPT_ROOT + "/_frontpagedescriptions.json", function(data) {
+    window.descriptions = data;
+  });
+}
+
+
+function loadSummary(button) {
+    var data = descriptions[button];
+    var summary = data.summary;
+    var htmlsummary = new Array();
+    for (i=0; i < summary.length; i++){
+      if (summary[i].length == 0) {
+        htmlsummary.push('<br/>');
+      }else{
+        htmlsummary.push(summary[i]);
+      }
+    }
+    $("#frontpageimage").attr("src",data.img);
+    $("#summary").html(htmlsummary.join(''));
+}
+
+
